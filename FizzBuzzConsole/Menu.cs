8edee0baa8db743b5a7efddbad06530f32153dfc
itishadi <113441103+itishadi@users.noreply.github.com>
2023-06-15 08:54:39 +0200

    Add project files.

---
+++ FizzBuzzConsole/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzConsole
+{
+    public class Menu
+    {
+        class MainProgram
+        {
+            static void Main(string[] args)
+            {
+                int choice;
+                string result = ""; // Flyttade deklarationen av result och gav den ett initialt värde
+
+                do
+                {
+                    Console.WriteLine("Välj ett alternativ:");
+                    Console.WriteLine("1. Anropa FizzBuzz för siffrorna 1-100");
+                    Console.WriteLine("2. Anropa FizzBuzz för en valfri siffra");
+                    Console.WriteLine("0. Avsluta");
+                    Console.Write("Val: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Resultat för siffrorna 1-100:");
+                            for (int i = 1; i <= 100; i++)
+                            {
+                                result = FizzBuzzApplication.FizzBuzz(i);
+                                Console.WriteLine(result);
+                            }
+                            break;
+                        case 2:
+                            Console.Write("Ange en siffra: ");
+                            int number = Convert.ToInt32(Console.ReadLine());
+                            result = FizzBuzzApplication.FizzBuzz(number);
+                            Console.WriteLine("Resultat: " + result);
+                            break;
+                        case 0:
+                            Console.WriteLine("Programmet avslutas.");
+                            break;
+                        default:
+                            Console.WriteLine("Ogiltigt val. Försök igen.");
+                            break;
+                    }
+
+                    Console.WriteLine();
+                } while (choice != 0);
+            }
+        }
+    }
+}
---
+++ FizzBuzzConsoleTest/FizzBuzzApplicationTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FizzBuzzConsole;
+
+namespace FizzBuzzApplicationTest
+{
+    [TestClass]
+    public class FizzBuzzTests
+    {
+        [TestMethod]
+        public void FizzBuzz_DivisibleBy3_ReturnsFizz()
+        {
+            // Arrange
+            int number = 9;
+
+            // Act
+            string result = FizzBuzzApplication.FizzBuzz(number);
+
+            // Assert
+            Assert.AreEqual("Fizz", result);
+        }
+
+        [TestMethod]
+        public void FizzBuzz_DivisibleBy5_ReturnsBuzz()
+        {
+            // Arrange
+            int number = 20;
+
+            // Act
+            string result = FizzBuzzApplication.FizzBuzz(number);
+
+            // Assert
+            Assert.AreEqual("Buzz", result);
+        }
+
+        [TestMethod]
+        public void FizzBuzz_DivisibleBy3And5_ReturnsFizzBuzz()
+        {
+            // Arrange
+            int number = 30;
+
+            // Act
+            string result = FizzBuzzApplication.FizzBuzz(number);
+
+            // Assert
+            Assert.AreEqual("FizzBuzz", result);
+        }
+
+        [TestMethod]
+        public void FizzBuzz_NotDivisibleBy3Or5_ReturnsNumberAsString()
+        {
+            // Arrange
+            int number = 7;
+
+            // Act
+            string result = FizzBuzzApplication.FizzBuzz(number);
+
+            // Assert
+            Assert.AreEqual("7", result);
+        }
+    }
+}
